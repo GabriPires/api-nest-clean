7ade784efb57895a4bac4cc4b202d5679396f36a
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-27 12:32:49 -0300

    feat: new prisma service to connect to database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get('hello')
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Get('prisma')
+  async getPrisma() {
+    return await this.prismaService.user.findMany()
   }
 }
