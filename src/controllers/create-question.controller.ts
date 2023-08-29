5b9f253510cf2bae1b79f1c4c2f301cd39da4745
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-29 17:50:20 -0300

    feat: new auth decorator to get current user payload

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user.decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/questions')
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
     return 'ok'
   }
 }
